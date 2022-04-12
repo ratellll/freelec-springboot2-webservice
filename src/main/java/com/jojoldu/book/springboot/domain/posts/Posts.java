56fd5c9808b597477c8d25473d128b84ba5116a0
ratellll <latellbin@naver.com>
2022-04-12 22:59:27 +0900

    도메인 시작부분

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -10,8 +10,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Getter
-@NoArgsConstructor
+@Getter // 클래스내에 모든 필드의 Getter 메소드 자동생성
+@NoArgsConstructor //기본 생성자 자동 추가
+                    //public Posts(){}와 같은 효과
 @Entity //테이블과 링크될 클래스를 나타냄
         //기본값으로 클래스의 캄멜케이스 이름을 언더스코어 네이밍(_)으로 테이블 이름을 매칭함
         //ex)SalesManager.java -> sales_manager table
@@ -34,7 +35,8 @@ public class Posts {
 
     private String author;
 
-    @Builder
+    @Builder //해당 클래스의 빌더 패턴 클래스를 생성
+            //생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
     public Posts(String title, String content, String author) {
         this.title = title;
         this.content = content;
---
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After //Juit에서 단위 테스트가 끝날 때마다 수행되는 메소드를 지정
+            //보통은 베포 전 전체 테스트를 수행 할 때 테스트간ㄴ 데이터 침범을 막기 위해 사용함
+            //여러 테스트가 동시에 수행되면 테스트용 데이터베이스인 H2에 데이터가 그대로 남아있어
+             // 다음 테스트 실행시 테스트가 실패할수있음
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시물저장_불러오기(){
+        //given
+        String title ="테스트게시글";
+        String content ="테스트본문";
+
+        postsRepository.save(Posts.builder() //테이블 posts에 insert/update 쿼리를실행 ,
+                                            // id값이 있다면 update 없다면 insert쿼리 실행
+                .title(title)
+                .content(content)
+                .author("jojoldu@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll(); //테이블 posts에 있는 모든 데이터를 조회하는 메소드
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+}
