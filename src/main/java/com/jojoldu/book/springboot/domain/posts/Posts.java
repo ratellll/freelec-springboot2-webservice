457eb365adeae2fb553c2981b21879cffb563bbb
ratellll <latellbin@naver.com>
2022-04-17 19:15:53 +0900

    index오류 해결 필요

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.domain.posts;
 
+import com.jojoldu.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,7 +17,7 @@ import javax.persistence.Id;
 @Entity //테이블과 링크될 클래스를 나타냄
         //기본값으로 클래스의 캄멜케이스 이름을 언더스코어 네이밍(_)으로 테이블 이름을 매칭함
         //ex)SalesManager.java -> sales_manager table
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id //해당 테이블의 PK 필드를 나타냄
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -41,5 +42,10 @@ public class Posts {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 }
