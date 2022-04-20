baa0ea8a5102dfbe0741a34a29721c37de76dca6
ratellll <latellbin@naver.com>
2022-04-20 13:53:40 +0900

    index오류 해결 필요!

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -8,7 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
