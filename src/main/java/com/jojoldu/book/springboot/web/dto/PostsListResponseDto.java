4d68486ec185d2b527edd0bba147047113037ea1
ratellll <latellbin@naver.com>
2022-06-09 16:20:33 +0900

    repository에 increaseHits추가

--- src/main/java/com/jojoldu/book/springboot/web/dto/PostsListResponseDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsListResponseDto.java
@@ -9,12 +9,14 @@ public class PostsListResponseDto {
     private Long id;
     private String title;
     private String author;
+    private int increaseHits;
     private LocalDateTime modifiedDate;
 
     public PostsListResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.author = entity.getAuthor();
+        this.increaseHits = entity.getHits();
         this.modifiedDate = entity.getModifiedDate();
     }
 }
--- src/main/java/com/jojoldu/book/springboot/web/dto/PostsResponseDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsResponseDto.java
@@ -11,11 +11,14 @@ public class PostsResponseDto {
     private String content;
     private String author;
 
+    private int increaseHits;
+
 
     public PostsResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.content = entity.getContent();
         this.author = entity.getAuthor();
+        this.increaseHits = entity.getHits();
     }
 }
